c901a203556d4097d084a08da69115bdb7c967c0
wolftech12 <142731245+wolftech12@users.noreply.github.com>
2023-10-12 22:01:03 +0300

    Delete escape sequences directory

--- escape sequences/main.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  //x++
-  //++x
-  int x = 5;
-  print(x++); // Post-increment: Prints the current value of x (5) and then increments it
-  print(x); // Prints the updated value of x after the post-increment (6)
-  print(++x); // Pre-increment: Increments x first (7) and then prints the updated value
-}
