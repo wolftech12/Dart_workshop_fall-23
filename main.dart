6d066ad6dd6b681fb6edd028b3b85c6927443e38
wolftech12 <142731245+wolftech12@users.noreply.github.com>
2023-10-12 21:46:05 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,19 @@
+void main() {
+  // * \n
+  // * \t
+  // * \r
+  // * \
+
+  print("Hello world"); //print Hello world
+
+  print(
+      "Hello\nworld"); // In this case, the text being output is "Hello" followed by a ***newline**** character ("\n") and then "world".
+
+  print(
+      "\tHello world"); // The "\t" is a special escape sequence that represents a horizontal ***tab***
+
+  print(
+      "Hello\rworld"); // the text being printed is "Hello" followed by a carriage return character ("\r") and then "world". The "\r" is a special escape sequence that represents a carriage return
+
+   print("Hello\world"); //is used as an escape character.
+}
